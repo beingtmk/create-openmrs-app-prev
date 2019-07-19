bc2a0b26db7054056c384ba6c71cfc0fc79d372e
Mithilesh Kohale <beingtmk@gmail.com>
2019-07-19 23:13:49 +0530

    renamed env to skeleton

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -6,32 +6,30 @@ import { Accordion } from '@openmrs/react-components';
 const AccordionComponent = props => {
   return (
     <div>
-      <Accordion title="HIV"> 
-        Human Immuno Deficiency Virus
-      </Accordion>
-      <Accordion title="Polio"> 
-        Caused by a Deadly Virus
-      </Accordion>
+      <Accordion title="HIV">Human Immuno Deficiency Virus & stuff</Accordion>
+      <Accordion title="Polio">Caused by a Deadly Virus</Accordion>
     </div>
-
   );
 };
 
 class App extends Component {
-
   getEnvValues() {
-    if (!process.env.REACT_APP_SERVER_ADDRESS || !process.env.REACT_APP_SERVER_CONTEXT_PATH) {
-      throw new Error('Please define `REACT_APP_SERVER_ADDRESS` and `REACT_APP_SERVER_CONTEXT_PATH` in your .env file');
+    if (
+      !process.env.REACT_APP_SERVER_ADDRESS ||
+      !process.env.REACT_APP_SERVER_CONTEXT_PATH
+    ) {
+      throw new Error(
+        'Please define `REACT_APP_SERVER_ADDRESS` and `REACT_APP_SERVER_CONTEXT_PATH` in your .env file'
+      );
     }
 
-    const serverAddress = process.env.REACT_APP_SERVER_ADDRESS
+    const serverAddress = process.env.REACT_APP_SERVER_ADDRESS;
     const serverContextPath = process.env.REACT_APP_SERVER_CONTEXT_PATH;
 
     return { serverAddress, serverContextPath };
   }
 
   render() {
-
     const { serverAddress, serverContextPath } = this.getEnvValues();
 
     return (
@@ -41,13 +39,14 @@ class App extends Component {
           <h2>Welcome to OpenMRS React Components</h2>
         </div>
         <p className="App-intro">
-           <b> Server Address: { serverAddress } </b><br/><br/>
-           <b> Server Context Path: { serverContextPath } </b>
+          <b> Server Address: {serverAddress} </b>
+          <br />
+          <br />
+          <b> Server Context Path: {serverContextPath} </b>
         </p>
 
         {/* Testing that OpenMRS-React-Component is working! */}
         <AccordionComponent />
-
       </div>
     );
   }
